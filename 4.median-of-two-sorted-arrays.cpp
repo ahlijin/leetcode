cbc96baf6913c1c528cbf685f51beebfa000207c
ahlijin <ahlijin@163.com>
2019-04-13 03:01:45 +0800

    zigzag-conversion

--- 4.median-of-two-sorted-arrays.cpp
+++ 4.median-of-two-sorted-arrays.cpp
@@ -11,18 +11,18 @@ public:
 			+ find_kth(nums1.begin(), m, nums2.begin(), n, total/2+1))/2.0;	
     }
 private:
-    static int find_kth(std::vector<int>::const_iterator nums1, int m,
-		    std::vector<int>::const_iterator nums2, int n, int k){
-	    if(m>n) return find_kth(nums2, n, nums1, m, k);		// m < n
+    static int find_kth(std::vector<int>::const_iterator nums1, int m, 
+            std::vector<int>::const_iterator nums2, int n, int k){
+        if(m>n) return find_kth(nums2, n, nums1, m, k);		// m < n
 	    if(m==0) return *(nums2+k-1);		// median of nums2
 	    if(k==1) return min(*nums1, *nums2);
 
 	    int ia = min(k/2, m), ib = k-ia;
 	    if(*(nums1+ia-1) < *(nums2+ib-1))
-			    return find_kth(nums1+ia, m-ia, nums2, n, k-ia);		// 删除nums1中前ia个数字
+            return find_kth(nums1+ia, m-ia, nums2, n, k-ia);		// 删除nums1中前ia个数字
 	    else if(*(nums1+ia-1) > *(nums2+ib-1))
-	    		    return find_kth(nums1, m, nums2+ib, n-ib, k-ib);		// 删除nums2中前ib个数字
+            return find_kth(nums1, m, nums2+ib, n-ib, k-ib);		// 删除nums2中前ib个数字
 	    else
-	    		    return nums1[ia-1];
-	}
+            return nums1[ia-1];
+    }
 };
